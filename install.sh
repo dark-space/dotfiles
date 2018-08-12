9a9f8f4c7530424549097078a8ceb498cca39d08
dark-space <moc.7iamg@gmail.com>
2018-08-13 02:00:59 +0900

    install.sh: first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+cd $HOME
+for f in dotfiles/.??*; do
+    if echo -n $f | grep '^dotfiles/.git' >/dev/null 2>&1; then
+        continue
+    fi
+    rm -f $(basename $f)
+    ln -s $f $(basename $f)
+done
