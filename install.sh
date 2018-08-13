c371eac85399ca06e1040c48e360565a3efe47d5
dark-space <user-dev@FolioG1.localdomain>
2018-08-13 18:30:52 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,38 @@
-cd $HOME
+#!/bin/sh 
 
-for f in dotfiles/.??*; do
-    if echo -n $f | grep '^dotfiles/.git' >/dev/null 2>&1; then
-        continue
+develop=false
+if [ $# -gt 0 ] && [ "$1" = "--dev" ]; then
+    develop=true
+fi
+
+dotfiles() {
+    cd $HOME
+    for f in dotfiles/.??*; do
+        if echo -n $f | grep '^dotfiles/.git' >/dev/null 2>&1; then
+            continue
+        fi
+        rm -f $(basename $f)
+        ln -s $f $(basename $f)
+    done
+}
+
+vim() {
+    cd $HOME
+    mkdir -p .vim/backup
+}
+
+cli() {
+    cd $HOME
+    if $develop; then
+        git clone https://github.com/dark-space/cli
+        mkdir -p cli/bin
+        sh cli/CompileAll.sh
+    else
+    echo "Not yet implemented"
     fi
-    rm -f $(basename $f)
-    ln -s $f $(basename $f)
-done
+}
 
-mkdir -p .vim/backup
 
+dotfiles
+vim
+cli
