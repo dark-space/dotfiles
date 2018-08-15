c532998ada008d9ec2e3f01dc42a52657ebd4595
dark-space <moc.7iamg@gmail.com>
2018-08-15 15:18:09 +0900

    Enter:execute, ctrl-e:end-of-line

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -4,6 +4,10 @@
 history_all=$HOME/.zsh/history_all.txt
 history_basedir=$HOME/.zsh/histories
 
+function __fzfcmd_dev() {
+    echo "$HOME/fzf"
+}
+
 function preexec_history() {
     cmd=$1
     echo "$cmd$(builtin pwd)" >> $history_all
@@ -12,20 +16,29 @@ function preexec_history() {
     echo "$cmd" >> $history_dir/history
 }
 
-fzf-history-widget() {
+function fzf-history-widget() {
     local history_file="${history_basedir}$(builtin pwd)/history"
     if [ -e $history_file ]; then
-        local selected=( \
+        IFS=$'\n' local out=( \
             $(tac $history_file | unique | \
-            FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=\"ctrl-r:toggle-sort\" $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" \
-            ./$(__fzfcmd)) \
+            FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=\"ctrl-r:toggle-sort\" $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m --expect=ctrl-e" \
+            $(__fzfcmd_dev)) \
         )
         local ret=$?
-        BUFFER="$selected"
-        zle redisplay
-        typeset -f zle-line-init >/dev/null && zle zle-line-init
-        zle accept-line
-        return $ret
+        local key=$(lines 1  <<< "$out")
+        local cmd=$(lines 2: <<< "$out")
+        if [ "$key" = "ctrl-e" ]; then
+            BUFFER="$cmd"
+            zle redisplay
+            typeset -f zle-line-init >/dev/null && zle zle-line-init
+            zle end-of-line
+            return $ret
+        else
+            BUFFER="$out"
+            zle redisplay
+            typeset -f zle-line-init >/dev/null && zle zle-line-init
+            zle accept-line
+        fi
     fi
 }
 zle -N fzf-history-widget
