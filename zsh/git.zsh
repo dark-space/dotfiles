86fffdf1c68ef423dad4fd6de62b2e7e6257ade5
dark-space <moc.7iamg@gmail.com>
2018-08-16 00:38:19 +0900

    git add with accept-line

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -4,16 +4,16 @@ function __fzfcmd_dev() {
 
 if which $(__fzfcmd_dev) >/dev/null 2>&1; then
     function fzf-git-widget() {
-        local selected=($(unbuffer git status -sb | $(__fzfcmd_dev) --reverse --ansi --preview="git diff --color=always {2..}" --preview-window=up:80% -m | columns -S 2:))
+        local selected=($(unbuffer git status -sb | $(__fzfcmd_dev) --reverse --ansi --preview="git diff --color=always {2..}" --preview-window=up:80% -m | sed -e 's/^ \+//' | columns -S 2:))
         if [[ "$selected" =~ \\S ]]; then
             BUFFER="git add $selected"
-            CURSOR=${#BUFFER}
             zle redisplay
             typeset -f zle-line-init >/dev/null && zle zle-line-init
+            zle accept-line
         fi
     }
     zle -N fzf-git-widget
-    bindkey "^g^i^t" fzf-git-widget
+    bindkey "^g^a" fzf-git-widget
 
     function fzf-git-log-widget() {
         local selected=($(git log --graph --decorate --oneline --abbrev=40 --color=always | $(__fzfcmd_dev) --reverse --ansi | grep -o '[0-9a-z]\{40\}'))
