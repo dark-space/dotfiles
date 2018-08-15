1a11ebce8685fa0eec28d825721ecadb42d84a0a
dark-space <moc.7iamg@gmail.com>
2018-08-15 16:09:38 +0900

    add history file of the session

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -3,6 +3,7 @@
 #
 history_all=$HOME/.zsh/history_all.txt
 history_basedir=$HOME/.zsh/histories
+history_session=""
 
 function __fzfcmd_dev() {
     echo "$HOME/fzf"
@@ -14,6 +15,7 @@ function preexec_history() {
     local history_dir="${history_basedir}$(builtin pwd)"
     mkdir -p $history_dir
     echo "$cmd" >> $history_dir/history
+    history_session+="\n"$cmd
 }
 
 if which $(__fzfcmd_dev) >/dev/null 2>&1; then
@@ -21,10 +23,14 @@ if which $(__fzfcmd_dev) >/dev/null 2>&1; then
         local history_file="${history_basedir}$(builtin pwd)/history"
         if [ -e $history_file ]; then
             IFS=$'\n' local out=( \
-                $(tac $history_file | unique | grep -i "^$LBUFFER" | \
+                $(history | sed -e 's/^\s*\S\+\s*//' | tac | unique | grep -i "^$LBUFFER" | \
                 FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=\"ctrl-r:toggle-sort\" $FZF_CTRL_R_OPTS +m --expect=ctrl-e" \
                 $(__fzfcmd_dev)) \
             )
+                #$(history | sed -e 's/^\s*\S\+\s*//' | tac | unique | grep -i "^$LBUFFER" | \
+                #$(tac $history_all | sed -e 's/.\+$//' | unique | grep -i "^$LBUFFER" | \
+                #$(echo -e $history_session | tac | unique | grep -i "^$LBUFFER" | \
+                #$(tac $history_file | unique | grep -i "^$LBUFFER" | \
             local ret=$?
             local key=$(lines 1  <<< "$out")
             local cmd=$(lines 2: <<< "$out")
