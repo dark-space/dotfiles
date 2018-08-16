a9b047bd776e7d501f754a041c59752afb60f43f
dark-space <moc.7iamg@gmail.com>
2018-08-16 16:58:20 +0900

    make 'git add' is a normal command; add 'git status'

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -3,17 +3,21 @@ function __fzfcmd_dev() {
 }
 
 if which $(__fzfcmd_dev) >/dev/null 2>&1; then
-    function fzf-git-widget() {
-        local selected=($(unbuffer git status -sb | $(__fzfcmd_dev) --reverse --ansi --preview="git diff --color=always {2..}" --preview-window=up:80% -m | grep -v '^##' | sed -e 's/^ \+//' | columns -S 2:))
+    function fzf-git-add() {
+        local selected=($(unbuffer git status -sb | $(__fzfcmd_dev) --reverse --ansi --preview="git diff --color=always {2..}" --preview-window=up:80% -m | grep -v '^##' | awk '{print $2}'))
         if [[ "$selected" =~ \\S ]]; then
-            BUFFER="git add $selected"
-            zle redisplay
-            typeset -f zle-line-init >/dev/null && zle zle-line-init
-            zle accept-line
+            git add $selected
         fi
     }
-    zle -N fzf-git-widget
-    bindkey "^g^a" fzf-git-widget
+    alias ga='fzf-git-add'
+
+    function fzf-git-branch() {
+        local selected=$(git branch --color=always | $(__fzfcmd_dev) --reverse --ansi)
+        if [[ "$selected" =~ \\S ]]; then
+            git checkout $selected
+        fi
+    }
+    alias gb='fzf-git-branch'
 
     function fzf-git-log-widget() {
         local selected=($(git log --graph --decorate --oneline --abbrev=40 --color=always | $(__fzfcmd_dev) --reverse --ansi | grep -o '[0-9a-z]\{40\}'))
@@ -26,5 +30,14 @@ if which $(__fzfcmd_dev) >/dev/null 2>&1; then
     }
     zle -N fzf-git-log-widget
     bindkey "^g^l" fzf-git-log-widget
+
+    function fzf-git-status-widget() {
+        local selected=$(unbuffer git status -sb | $(__fzfcmd_dev) --reverse --ansi --preview="git diff --color=always {2..}" --preview-window=up:80%)
+    }
+    zle -N fzf-git-status-widget
+    bindkey "^g^s" fzf-git-status-widget
+else
+    alias ga='git add'
+    alias gb='git branch; git chechout'
 fi
 
