5db7229d2134e4f9b64889633e4fb190665c13d0
dark-space <moc.7iamg@gmail.com>
2018-08-17 16:07:40 +0900

    makes history file selectable

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -10,7 +10,7 @@ function __fzfcmd_dev() {
 }
 
 function preexec_history() {
-    cmd=$(shownl -t <<< $1)
+    cmd=$(newline -z -r="\\n" <<< $1)
     cat <<< "$cmd$(builtin pwd)" >> $history_all
     local history_dir="${history_basedir}$(builtin pwd)"
     mkdir -p $history_dir
@@ -19,33 +19,60 @@ function preexec_history() {
 }
 
 if which $(__fzfcmd_dev) >/dev/null 2>&1; then
+    function read_history() {
+        local type=$1
+        if [ "$type" = "history" ]; then
+            history | sed -e 's/^\s*\S\+\s*//' | tac | unique | grep -i "^$LBUFFER"
+        elif [ "$type" = "directory" ]; then
+            local history_here="${history_basedir}$(builtin pwd)/history"
+            if [ ! -e $history_here ]; then
+                mkdir -p $(dirname $history_here)
+                touch $history_here
+            fi
+            tac $history_here | unique | grep -i "^$LBUFFER"
+        elif [ "$type" = "all" ]; then
+            tac $history_all | sed -e 's//\t[33m/' -e 's/$/[0m/' | unique | grep -i "^$LBUFFER"
+        elif [ "$type" = "session" ]; then
+            echo -e $history_session | tac | unique | grep -i "^$LBUFFER"
+        fi
+    }
+
     function fzf-history-widget() {
-        local history_file="${history_basedir}$(builtin pwd)/history"
-        if [ -e $history_file ]; then
-            IFS=$'\n' local out=( \
-                $(tac $history_file | unique | grep -i "^$LBUFFER" | \
-                FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=\"ctrl-r:toggle-sort\" $FZF_CTRL_R_OPTS +m --expect=ctrl-e" \
-                $(__fzfcmd_dev)) \
-            )
-                #$(history | sed -e 's/^\s*\S\+\s*//' | tac | unique | grep -i "^$LBUFFER" | \
-                #$(tac $history_all | sed -e 's/.\+$//' | unique | grep -i "^$LBUFFER" | \
-                #$(echo -e $history_session | tac | unique | grep -i "^$LBUFFER" | \
-                #$(tac $history_file | unique | grep -i "^$LBUFFER" | \
+        local history_type=${HISTORY_TYPE:-"all"}
+        while IFS=$'\n' local out=( \
+            $(read_history $history_type | \
+            FZF_DEFAULT_OPTS="--ansi --height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index $FZF_CTRL_R_OPTS +m --expect=ctrl-a,ctrl-e,ctrl-r,ctrl-d,ctrl-s,ctrl-h" \
+            $(__fzfcmd_dev)) \
+        ); do
             local ret=$?
             local key=$(lines 1  <<< "$out")
-            if [ "$key" = "ctrl-e" ]; then
+            if [ "$key" = "ctrl-a" ]; then
+                BUFFER=$(lines 2: <<< "$out" | sed -e 's/\\n/\n/g')
+                zle redisplay
+                typeset -f zle-line-init >/dev/null && zle zle-line-init
+                return $ret
+            elif [ "$key" = "ctrl-e" ]; then
                 BUFFER=$(lines 2: <<< "$out" | sed -e 's/\\n/\n/g')
                 CURSOR=${#BUFFER}
                 zle redisplay
                 typeset -f zle-line-init >/dev/null && zle zle-line-init
                 return $ret
+            elif [ "$key" = "ctrl-r" ]; then
+                history_type="all"
+            elif [ "$key" = "ctrl-d" ]; then
+                history_type="directory"
+            elif [ "$key" = "ctrl-s" ]; then
+                history_type="session"
+            elif [ "$key" = "ctrl-h" ]; then
+                history_type="history"
             else
                 BUFFER=$(sed -e 's/\\n/\n/g' <<< "$out")
                 zle redisplay
                 typeset -f zle-line-init >/dev/null && zle zle-line-init
                 zle accept-line
+                break
             fi
-        fi
+        done
     }
     zle -N fzf-history-widget
     bindkey "^r" fzf-history-widget
