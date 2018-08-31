d88b203dcd89185765b88d24a7d1829de61b87b9
dark-space <moc.7iamg@gmail.com>
2018-08-31 13:44:29 +0900

    update details

--- zsh/lib/preview-git.zsh
+++ zsh/lib/preview-git.zsh
@@ -6,7 +6,12 @@ if [ $# -ge 2 ] && [[ "$1" =~ ^-\d+ ]]; then
     shift
 fi
 
-git diff --color=always "$1" | grep '.'
+if grep '\]$' <<< "$1" >/dev/null 2>&1; then
+    echo -n ""
+    return
+fi
+
+git diff --color=always "$1" | head $n | grep '.'
 if [ $? -ne 0 ]; then
     head $n "$1"
 fi
